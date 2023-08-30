4b464995db29e2cd91fe314cd1a72bf901393def
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-08-30 19:51:51 +0300

    add prsima to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
+import { PrismaService } from './prisma/prisma.service';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [],
   controllers: [AppController, UserController, CatsController],
-  providers: [AppService, CatsService],
+  providers: [AppService, CatsService, PrismaService, UserService],
 })
 export class AppModule {}
